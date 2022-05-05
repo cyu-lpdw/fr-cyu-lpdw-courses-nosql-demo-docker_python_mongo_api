09e9ef49a2852bdb4e78a330a285890792c9706d
François Michaud <fmichaud@deepnox.io>
2022-05-05 23:14:02 +0200

    Prepare unit tests of logic layer

--- test/cyu/tests/lpdw/pymo/services_test.py
+++ test/cyu/tests/lpdw/pymo/services_test.py
@@ -2,24 +2,37 @@
 
 """
 """
+
 import pytest
 from mongomock_motor import AsyncMongoMockClient
+
+from cyu.lpdw.pymo.services import UsersService
+
+
+users_coll = None 
+""" Database user collection. """
+
+users_service = None
+""" Users logic service. """
 
 @pytest.fixture()
 def resource(request):
     print("setup")
-
+    conn = AsyncMongoMockClient()['tests']
+    users_coll = conn.get("users")
+    users_service = UsersService(conn)
     def teardown():
         print("teardown")
     request.addfinalizer(teardown)
     
     return "resource"
 
-class TestClass:
+class ServicesTestClass:
 
   
-  async def test_mock_client():
-    collection = AsyncMongoMockClient()['tests']['test-1']
+  async def test_create():
+    """ Unit test: create a new user.
+    """
     assert await collection.find({}).to_list(None) == []
     result = await collection.insert_one({'a': 1})
     assert result.inserted_id
